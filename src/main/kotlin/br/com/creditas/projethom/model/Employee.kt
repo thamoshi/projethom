2004395c2498793779c4214641571da7b7c32eac
Thomas <thomasoshima97@gmail.com>
2022-07-11 14:08:34 -0300

    Add Employee entity

---
+++ src/main/kotlin/br/com/creditas/projethom/model/Employee.kt
@@ -0,0 +1,20 @@
+package br.com.creditas.projethom.model
+
+import java.time.LocalDateTime
+import java.util.*
+import javax.persistence.Id
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.ManyToOne
+
+data class Employee(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long,
+  val personId: UUID? = null,
+  @ManyToOne
+  val team: Team,
+  val role: Role,
+  val createdAt: LocalDateTime = LocalDateTime.now(),
+  var updatedAt: LocalDateTime = LocalDateTime.now()
+)

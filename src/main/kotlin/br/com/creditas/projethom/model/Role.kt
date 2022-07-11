d465a2a65c6814b632b850feff4c3346095633c3
Thomas <thomasoshima97@gmail.com>
2022-07-11 14:11:50 -0300

    Add Role enum

---
+++ src/main/kotlin/br/com/creditas/projethom/model/Role.kt
@@ -0,0 +1,10 @@
+package br.com.creditas.projethom.model
+
+enum class Role {
+  PRODUCT_MANAGER,
+  ENGINEERING_MANAGER,
+  ENGINEERING_LEAD,
+  SPECIALIST_ENGINEER,
+  SOFTWARE_ENGINEER,
+  INTERN
+}

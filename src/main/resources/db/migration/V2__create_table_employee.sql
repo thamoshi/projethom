618e0b43505c4a2c42ddf8372e04d1ee56c5a35c
Thomas <thomasoshima97@gmail.com>
2022-07-21 15:48:40 -0300

    fix: personId can be null

--- src/main/resources/db/migration/V2__create_table_employee.sql
+++ src/main/resources/db/migration/V2__create_table_employee.sql
@@ -1,7 +1,7 @@
 create table employee(
     id UUID not null,
-    person_id UUID not null,
-    team_id UUID,
+    person_id UUID,
+    team_id UUID not null,
     role varchar(50) not null,
     created_at date not null,
     updated_at date not null,

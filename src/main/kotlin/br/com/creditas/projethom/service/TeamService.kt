cce6446b4057c8fc643ab309d10a4455f099dd7f
Thomas <thomasoshima97@gmail.com>
2022-07-21 15:51:42 -0300

    feat: PUT and DELETE

--- src/main/kotlin/br/com/creditas/projethom/service/TeamService.kt
+++ src/main/kotlin/br/com/creditas/projethom/service/TeamService.kt
@@ -2,6 +2,8 @@ package br.com.creditas.projethom.service
 
 import br.com.creditas.projethom.dto.TeamRequest
 import br.com.creditas.projethom.dto.TeamResponse
+import br.com.creditas.projethom.dto.UpdateTeamRequest
+import br.com.creditas.projethom.model.Tribe
 import org.springframework.stereotype.Service
 import br.com.creditas.projethom.repository.TeamRepository
 import java.util.UUID
@@ -11,9 +13,24 @@ class TeamService(
     private val teamRepository: TeamRepository
 ) {
 
-    fun teamList(): List<TeamResponse> {
-        return teamRepository.findAll().map {
-            TeamResponse.fromEntity(it)
+    fun teamList(tribe: String?): List<TeamResponse> {
+        if (tribe == null) {
+            return teamRepository.findAll().map{
+                TeamResponse.fromEntity(it)
+            }
+        } else {
+            return try {
+                val teams = teamRepository.findByTribe(
+                    tribe.let {
+                        Tribe.valueOf(tribe.uppercase())
+                    }
+                )
+                teams.map {
+                    TeamResponse.fromEntity(it)
+                }
+            } catch (e: IllegalArgumentException) {
+                emptyList()
+            }
         }
     }
 
@@ -28,6 +45,23 @@ class TeamService(
         val team = TeamRequest.toEntity(teamRequest)
         teamRepository.save(team)
         return TeamResponse.fromEntity(team)
+    }
+
+    fun updateTeam(
+        updateTeamRequest: UpdateTeamRequest
+    ): TeamResponse {
+        val team = teamRepository.findById(updateTeamRequest.id).orElseThrow()
+        team.name = updateTeamRequest.name
+        team.description = updateTeamRequest.description
+        team.tribe = updateTeamRequest.tribe
+        teamRepository.save(team)
+        return TeamResponse.fromEntity(team)
+    }
+
+    fun deleteTeamById(
+        id: UUID
+    ) {
+        teamRepository.deleteById(id)
     }
 
 }

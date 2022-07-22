ff46b598b53dc02377a494fc9ce2ae39f44a716f
Thomas <thomasoshima97@gmail.com>
2022-07-22 10:03:05 -0300

    fix: no wild import

--- src/main/kotlin/br/com/creditas/projethom/model/Employee.kt
+++ src/main/kotlin/br/com/creditas/projethom/model/Employee.kt
@@ -2,7 +2,11 @@ package br.com.creditas.projethom.model
 
 import java.time.LocalDateTime
 import java.util.UUID
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+import javax.persistence.Enumerated
+import javax.persistence.EnumType
 
 @Entity
 data class Employee(
